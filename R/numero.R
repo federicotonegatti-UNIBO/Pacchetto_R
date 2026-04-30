b0143d8abaf7b84299a58a81241ac4488f8bab4f
federicotonegatti-UNIBO <federico.tonegatti@gmail.com>
2026-04-30 15:01:15 +0200

    Create numero.R

---
+++ R/numero.R
@@ -0,0 +1,11 @@
+numero <- function(x){
+  if(x>0){
+    print("Questo numero è positivo, ciuco!")
+  }
+  else if(x<0) {
+    print("Questo numero è negativo, se non sai queste cose, torna a scuola")  
+  }
+  else {
+    print("Zero non è né negativo né positivo") 
+  }
+}
